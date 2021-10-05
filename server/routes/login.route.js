7e86756757446a8621112a0d1f5a0992fbf55e78
Prateek Sood <prateeksood123@gmail.com>
2021-10-06 03:25:36 +0530

    sessions

--- server/routes/login.route.js
+++ server/routes/login.route.js
@@ -2,7 +2,7 @@ const router=require("express").Router();
 const path = require("path");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const User = require("../models/User");
+const User = require("../models/User.model");
 
 router.get("/", function (request, response) {
   response.sendFile(path.join(__dirname, "../client/login/login.html"));
@@ -20,12 +20,12 @@ router.post("/", async function (request, response) {
       if (foundUser.password !== password)
         response.status(401).send("Invalid password");
       else {
-        jwt.sign({_id:foundUser._id},process.env.JWT_SECRET,{expiresIn:tokenExpiry},(err,token)=>{
+        foundUser=foundUser._doc;
+        delete foundUser.password;
+        jwt.sign(foundUser,process.env.JWT_SECRET,{expiresIn:tokenExpiry},(err,token)=>{
           if(err)
             response.status(500).send(`Something went wrong : ${err.message}`);
           else{
-            foundUser=foundUser._doc;
-            delete foundUser.password;
             foundUser={...foundUser,token}
             response.status(200).json(foundUser);
           }
