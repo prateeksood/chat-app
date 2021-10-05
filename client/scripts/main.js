35a335aca4dc7a9d0ef6048307b198bd45c594c4
Rameet Mashta <rameetmashta@gmail.com>
2021-10-05 14:03:29 +0530

    Form validation

---
+++ client/scripts/main.js
@@ -0,0 +1,72 @@
+/// <reference path="dom.js"/>
+
+const App={
+  element:{
+    containerChat:DOM.id("chat-container"),
+    containerLogin:DOM.id("login-container"),
+    formLogin:DOM.id("login-form"),
+    formRegister:DOM.id("register-form"),
+    holderResponse:DOM.id("message-holder")
+  },
+  dataValidation:{
+    email:/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+    username:/^(?=[a-zA-Z0-9._]{4,20}$)(?!.*[_.]{2})[^_.].*[^_.]$/,
+    password:/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&._])[A-Za-z\d@$!%*#?&._]{6,50}$/
+  },
+  alert(...messages){
+    // console.log(...messages);
+    App.element.holderResponse.innerHTML=messages.join("<br/>");
+  }
+};
+
+App.element.formLogin.addEventListener("submit",async event=>{
+  event.preventDefault();
+  const formData=new FormData(event.target);
+  const request=await fetch("/login",{
+    method:"POST",
+    body:new URLSearchParams(formData)
+  }).catch(ex=>App.alert("ERROR: ",ex));
+  if(request)
+    App.alert(await request.text());
+});
+
+App.element.formRegister.addEventListener("submit",async event=>{
+  event.preventDefault();
+  const formData=new FormData(event.target);
+  const email=formData.get("email");
+  const username=formData.get("username");
+  const password=formData.get("password");
+  const confirmPassword=formData.get("confirmPassword");
+  if(!App.dataValidation.email.test(email)){
+    App.alert("Invalid email address")
+    return;
+  }
+  if(!App.dataValidation.username.test(username)){
+    App.alert("Invalid username. Requirements: Minimum 4 characters, maximum 20 characters, numbers and letters are allowed, only special characters allowed are . (dot) and _ (underscore)")
+    return;
+  }
+  if(App.dataValidation.password.test(password)){
+    if(password!==confirmPassword){
+      App.alert("Password does not match");
+      return;
+    }
+  }else{
+    App.alert("Invalid password. Requirements: Minimum 6 characters, maximum 50 characters, at least one letter, one number and one special character ( @ $ ! % * # ? & . _ )");
+    return;
+  }
+  const request=await fetch("/register",{
+    method:"POST",
+    body:new URLSearchParams(formData)
+  }).catch(ex=>App.alert("Error: ",ex));
+  if(request)
+    App.alert(await request.text());
+});
+
+App.element.formLogin.addEventListener("reset",event=>{
+  event.preventDefault();
+  event.target.closest("#form-holder").removeAttribute("active");
+});
+App.element.formRegister.addEventListener("reset",event=>{
+  event.preventDefault();
+  event.target.closest("#form-holder").setAttribute("active","login");
+});
--- server/index.js
+++ server/index.js
@@ -21,12 +21,12 @@ app.post("/login",async function(request,response){
     const {username,password} = request.body;
     let foundUser=await User.findOne({username});
     if(!foundUser)
-      response.status(200).send('User does not exists');
+      response.status(200).send('User does not exist');
     else{
-      if(foundUser.password!=password)
+      if(foundUser.password!==password)
         response.status(200).send('Invalid password');
       else{
-        foundUser.password = undefined;
+        delete foundUser.password;
         response.status(200).send(foundUser);
       }
     }
@@ -35,13 +35,18 @@ app.post("/login",async function(request,response){
   catch(err){
     response.status(500).send(err);
   }
-  
+
 });
+
+const dataValidation={
+  email:/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+  username:/^(?=[a-zA-Z0-9._]{4,20}$)(?!.*[_.]{2})[^_.].*[^_.]$/,
+  password:/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,20}$/
+};
 
 app.post("/register",async function(request,response){
   try{
-
-    const {fullName,email,password,username} = request.body;
+    const {fullName,email,password,confirmPassword,username} = request.body;
     let existingUser=await User.findOne({email});
     let existingUsername=await User.findOne({username});
     if(existingUser){
@@ -50,10 +55,27 @@ app.post("/register",async function(request,response){
     else if(existingUsername){
       response.status(200).send('Username already taken, try again with different username');
     }
+    else if(!dataValidation.username.test(username)){
+      response.status(200).send("Invalid username, Requirements: Minimum 4 characters, maximum 20 characters, numbers and letters are allowed, only special characters allowed are . (dot) and _ (underscore)");
+    }
+    else if(!dataValidation.email.test(email)){
+      response.status(200).send("Invalid email adress");
+    }
+    else if(!dataValidation.password.test(password)){
+      response.status(200).send("Invalid password. Requirements: Minimum 6 characters, maximum 50 characters, at least one letter, one number and one special character");
+    }
+    else if(password!==confirmPassword){
+      response.status(200).send("Password does not match");
+    }
     else{
-      let newUser = await new User({name:fullName,email,password,username});
+      let newUser = await new User({
+        name:fullName,
+        email,
+        password,
+        username:username.toLowerCase()
+      });
       let savedUser=await newUser.save();
-      newUser.password=undefined;
+      delete newUser.password;
       response.status(200).send(newUser);
     }
   }
