3be0ef9b2b46d35b0edfb461cc416c4f416c9b4e
Prateek Sood <prateeksood123@gmail.com>
2022-01-05 20:45:43 +0530

    socket linked

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -1,5 +1,6 @@
 const MessageService = require("../services/message.service");
 const ChatService = require("../services/chat.service");
+// const connections = require("../socket")
 module.exports = class MessageController {
 
   /**
@@ -42,10 +43,17 @@ module.exports = class MessageController {
       const savedMessage = await MessageService.saveNewMessage({ content, sender, reference, chat: chatID });
       if (!savedMessage)
         throw "Unable to save message";
-      await ChatService.findChatByIdAndUpdate(chatID, { messages: savedMessage._id }, "push");
-
+      const chat = await ChatService.findChatByIdAndUpdate(chatID, { messages: savedMessage._id }, "push");
+      chat.participants.forEach(user => {
+        if (user.user in global.connections && !user.user.equals(sender)) {
+          global.connections[user.user].send(JSON.stringify({
+            error: null, type: "message", data: { message: savedMessage }
+          }));
+        }
+      });
       response.status(200).json(savedMessage);
     } catch (ex) {
+      console.log(ex);
       response.status(500).json({ message: `Something went wrong: ${ex.message}` });
     }
   }
