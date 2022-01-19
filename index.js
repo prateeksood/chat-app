991eabf59da0130b45bf1a307f4561f1064a6ba3
Prateek Sood <prateeksood123@gmail.com>
2022-01-19 19:55:14 +0530

    temp commit

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use(express.urlencoded({
 app.use(express.json());
 app.use("/", express.static(__dirname + "/./client"));
 app.use("/resources/profilePictures", express.static(__dirname + "/./public/uploads/profilePictures"));
+app.use("/resources/illustrations", express.static(__dirname + "/./public/Resources/illustrations"));
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/../index.html");
